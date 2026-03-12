26da2cbfdd743a52252fb3edf51bd39c48a0bc16
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-03-11 21:31:08 -0500

    add DTOs and team mapping

--- SportsLeague.DataAccess/obj/Debug/net10.0/SportsLeague.DataAccess.AssemblyInfo.cs
+++ SportsLeague.DataAccess/obj/Debug/net10.0/SportsLeague.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c595e29672b23f93a98c96f71e8e4377207ca5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
