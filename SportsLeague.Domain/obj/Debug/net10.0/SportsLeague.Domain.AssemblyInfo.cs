7b507bfa09afd05adede176258e84be7e0d6ddec
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-04-01 19:48:34 -0500

    Add Sponsor entities and enums to domain model
    Added Sponsor and TournamentSponsor entities to support sponsor management and tournament associations. Introduced SponsorCategory enum.

--- SportsLeague.Domain/obj/Debug/net10.0/SportsLeague.Domain.AssemblyInfo.cs
+++ SportsLeague.Domain/obj/Debug/net10.0/SportsLeague.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsLeague.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3caebf62fcfb5eb38c6530c629dc7527b04398a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac277d3ecd80e3a0da8e89bf72266fdce45b29f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsLeague.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsLeague.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
