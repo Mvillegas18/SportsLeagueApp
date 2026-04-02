f9fe81ba5eaed74bdd7a8d92d7e1c1c0b795f937
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-04-01 20:05:07 -0500

    Update assemblies to reference latest source commit

--- SportsLeague.DataAccess/obj/Debug/net10.0/SportsLeague.DataAccess.AssemblyInfo.cs
+++ SportsLeague.DataAccess/obj/Debug/net10.0/SportsLeague.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae4aad6d373565b6610adae95882633295019a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9348cb608dc1b025b03ce38406b0cd95b6d1a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsLeague.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
