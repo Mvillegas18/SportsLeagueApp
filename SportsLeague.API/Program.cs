8a2a82defdb84a73c68dc8cd7d159c0f720f5334
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-05-04 18:26:02 -0500

    Add DB migration and seeding on app startup
    Refactored Program.cs to run database migrations and seed initial data during application startup. Cleaned up unused using directives. Updated assembly info for new build/version.

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -1,15 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-
 using SportsLeague.DataAccess.Context;
-
 using SportsLeague.DataAccess.Repositories;
-
-using SportsLeague.Domain.Interfaces.Repositories;
-
-using SportsLeague.Domain.Interfaces.Services;
-
-using SportsLeague.Domain.Services;
+using SportsLeague.DataAccess.Seeders;
 using SportsLeague.Domain.Helpers;
+using SportsLeague.Domain.Interfaces.Repositories;
+using SportsLeague.Domain.Interfaces.Services;
+using SportsLeague.Domain.Services;
+using SportsLeague.DataAccess.Seeders;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -78,6 +75,24 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+// ── Data Seeder ── 
+
+using (var scope = app.Services.CreateScope())
+
+{
+
+    var context = scope.ServiceProvider
+
+        .GetRequiredService<LeagueDbContext>();
+
+
+
+    await context.Database.MigrateAsync(); // Crea la BD + aplica migraciones 
+
+    await DataSeeder.SeedAsync(context);
+
+}
 
 
 // ── Middleware Pipeline ──
